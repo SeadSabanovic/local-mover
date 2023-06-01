c6b4dd6bda743912536d698dc7a3f0a13b242bb1
s.sabanovic <s.sabanovic@agentur-loop.com>
2023-06-01 11:22:55 +0200

    goba brat

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Filter {
+  active: boolean;
+  type: string;
+  icon: string;
+  color: string;
+  iconBg: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  iconAssetsPath = 'assets/icons/transport-';
+  cards: Filter[] = [
+    {
+      active: true,
+      type: 'All',
+      icon: `${this.iconAssetsPath}all.svg`,
+      color: '#0096F2',
+      iconBg: '#D2E8FE',
+    },
+    {
+      active: false,
+      type: 'Shuttle Bus',
+      icon: `${this.iconAssetsPath}bus.svg`,
+      color: '#F36887',
+      iconBg: '#FEF0F3',
+    },
+    {
+      active: false,
+      type: 'Train',
+      icon: `${this.iconAssetsPath}train.svg`,
+      color: '#9481FF',
+      iconBg: '#F4F2FF',
+    },
+    {
+      active: false,
+      type: 'Trolley Bus',
+      icon: `${this.iconAssetsPath}trolley.svg`,
+      color: '#06A656',
+      iconBg: '#E6F6EE',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  changeFilter(card: Filter) {
+    card.active = !card.active;
+    // Refresh here
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
